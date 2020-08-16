9b5f1bbf27fe3f334768e4b117472d8bb8200bb8
StorieswithSadie <69767354+StorieswithSadie@users.noreply.github.com>
2020-08-16 14:56:30 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ console.clear();
 
 
 //ADD VIDEO IDS TO THE IDLIST ARRAY, IN THE FORMAT ,"[videoID]"
-var idList = ["wDrrG3IukbM", "qANUKka9-UA", "ieGCkFmTQ80", "u-D-j94uipo", "n30i9lIrg9I"];
+var idList = ["wDrrG3IukbM", "qANUKka9-UA", "ieGCkFmTQ80", "u-D-j94uipo", "n30i9lIrg9I", "5qap5aO4i9A"];
 
 var videoPosters;
 var node;
