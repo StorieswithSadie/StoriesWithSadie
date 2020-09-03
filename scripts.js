3b6d4ce3b89dab0feb0aa6594ef648b5ef882427
StorieswithSadie <69767354+StorieswithSadie@users.noreply.github.com>
2020-09-03 00:11:44 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -44,7 +44,9 @@ function search(e) {
     searchTerm = e.value;
     searchTerm = searchTerm.toLowerCase();
      for (i = 0; i < idList.length; i++) {
-        if (titleList[i].toLowerCase().indexOf(searchTerm) + authorList[i].toLowerCase().indexOf(searchTerm)  + readerList[i].toLowerCase().indexOf(searchTerm) + genreList[i].toLowerCase().indexOf(searchTerm) > -4) {
+        if (titleList[i].toLowerCase().indexOf(searchTerm) + authorList[i].toLowerCase().indexOf(searchTerm) > -2) {
+            document.getElementById(idList[i]).style.display = "inline-block";
+        } else if (readerList[i].toLowerCase().indexOf(searchTerm) + genreList[i].toLowerCase().indexOf(searchTerm) > -2) {
             document.getElementById(idList[i]).style.display = "inline-block";
         } else {
             document.getElementById(idList[i]).style.display = "none";
