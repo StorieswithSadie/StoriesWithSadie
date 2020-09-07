9b525488f737609a2323e900e92114a5e784631e
StorieswithSadie <69767354+StorieswithSadie@users.noreply.github.com>
2020-09-07 00:36:45 -0400

    Attempted rework
    Possibly missing a matching length of readerList...?

--- scripts.js
+++ scripts.js
@@ -2,20 +2,27 @@ console.clear();
 
 //ADD VIDEO IDS TO THE IDLIST ARRAY, IN THE FORMAT ,"[videoID]"
 var idList = ["MG8WbK800U4", "AyiSoYyXpwQ", "Hi_qaGwi7jo", "gGDTGoDRy4Y", "kzde5cAihnE", "xS8S6RzRM_k", "bvkxj3VRoWs", "5_tlAu0Z5s8", "8lNJZ-C8_ik", "4n6-7NA9LNc", "GPWrmekih9g",
-             "CWl2PRpAP_s", "CSvF7UD1loU", "zM-6us2rm94"];
+             "CWl2PRpAP_s", "CSvF7UD1loU", "zM-6us2rm94", "2dF-d8XDsGA", "Xbu0XFbHPfA", "l68PHV6uQ1I", "9JZ07vPLv6o"];
 var titleList = ["The Giving Tree", "Green Eggs and Ham", "Rain", "Room on the Broom", "What a Funny Bunny", "Oh, the Places You'll Go", "Music of the Meadow", "The Colors of Fall",
-                 "Birds", "Fox in Socks", "Wocket in my Pocket", "Goodnight Moon", "The King, the Mice, and the Cheese", "The Mitten"];
+                 "Birds", "Fox in Socks", "Wocket in my Pocket", "Goodnight Moon", "The King, the Mice, and the Cheese", "The Mitten", "Elephant and Piggie: I Will Take a Nap", 
+                 "Elephant and Piggie: I Got Invited To a Party", "Baby Bear, Baby Bear, What Do You See?", "The Whales Go By"];
 var authorList = ["Shel Silverstein", "Dr. Seuss", "Wonder Starters", "Julia Donaldson Axel Scheffler", "ABC Adventures", "Dr. Seuss", "Susan Ring", "Leslie Eckard", "Wonder Starters",
-                  "Dr. Seuss", "Dr. Seuss", "Margaret Brown", "Nancy and Eric Gurney", "Jan Brett"];
+                  "Dr. Seuss", "Dr. Seuss", "Margaret Brown", "Nancy and Eric Gurney", "Jan Brett", "Mo Willems", "Mo Willems", "Bill Martin Jr.", "Fred Phleger"];
 var readerList = ["Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg",
-                 "Sadie Sahhlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg"];
-var genreList = ["Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction"];
+                 "Sadie Sahhlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg", "Sadie Sahlberg"];
+var genreList = ["Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction", "Fiction"];
 
 var videoPosters;
 var node;
 var address;
 
 function imageGen() {
+  
+    //Alert if wrong list lengths have been declared. Users should NEVER see this alert, fix the issues
+    if (idList.length + titleList.length + authorList.length + readerList.length + genreList.length != idList.length * 5) {
+        alert("Data Error: Mismatching list lengths. This is a temporary issue. Error Code: " + idList.length + titleList.length + authorList.length + readerList.length + genreList.length);
+    }
+  
     //DYNAMICALLY CREATE TD ELEMENTS WITH IDLIST
     for (i = 0; i < idList.length; i++) {
         node = document.createElement("TD");
@@ -30,10 +37,6 @@ function imageGen() {
     for (i = 0; i < videoPosters.length; i++) {
         videoPosters[i].style.backgroundImage = "url('https://img.youtube.com/vi/" + videoPosters[i].id + "/sddefault.jpg')";
         videoPosters[i].innerHTML = titleList[i] + " by " + authorList[i];
-    }
-    
-    if (idList.length + titleList.length + authorList.length + readerList.length + genreList.length != idList.length * 5) {
-        alert("Data Error: Mismatching list lengths. This is a temporary issue. Error Code: " + idList.length + titleList.length + authorList.length + readerList.length + genreList.length);
     }
 
 }
